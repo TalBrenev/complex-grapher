519f397c1f0a972e9d5bbdbb14b595381c2073b5
Tal Brenev <tal.brenev@gmail.com>
2019-02-10 22:23:11 -0500

    Create tests for basic arithmetic of complex numbers

--- test/complex_grapher/test_runner.cljs
+++ test/complex_grapher/test_runner.cljs
@@ -1,7 +1,9 @@
 (ns complex-grapher.test-runner
   (:require
     [cljs-test-display.core]
-    [figwheel.main.testing :refer [run-tests]]))
+    [figwheel.main.testing :refer [run-tests]]
+
+    [complex-grapher.complex-arithmetic-test]))
 
 (defn -main [& args]
   (run-tests (cljs-test-display.core/init! "test-results")))
