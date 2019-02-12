c29407ff4f8446e1121eefb9d1c2f13a4402aa7c
Tal Brenev <tal.brenev@gmail.com>
2019-02-11 20:48:37 -0500

    Create macros for testing approximate equality of numbers

--- test/complex_grapher/complex_arithmetic_test.cljs
+++ test/complex_grapher/complex_arithmetic_test.cljs
@@ -1,46 +1,46 @@
 (ns complex-grapher.complex-arithmetic-test
-  (:require [cljs.test :refer [deftest testing is are]]
+  (:require [cljs.test :refer [deftest testing]]
             [complex-grapher.complex-arithmetic :refer [complex-from-cartesian
                                                         complex-from-polar
                                                         re im arg mag
                                                         add sub mul div]])
-  (:require-macros [complex-grapher.test-utils :refer [close?]]))
+  (:require-macros [complex-grapher.test-utils :refer [are-close]]))
 
 (deftest complex-arithmetic
   (testing "create a complex number"
     (testing "from caretesian coordinates"
       (let [z (complex-from-cartesian 0 0)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  0
           (arg z) 0
           (mag z) 0))
       (let [z (complex-from-cartesian 1 0)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  1
           (im z)  0
           (arg z) 0
           (mag z) 1))
       (let [z (complex-from-cartesian 0 1)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  1
           (arg z) (/ Math/PI 2)
           (mag z) 1))
       (let [z (complex-from-cartesian -1 0)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  -1
           (im z)  0
           (arg z) Math/PI
           (mag z) 1))
       (let [z (complex-from-cartesian 0 -1)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  -1
           (arg z) (- (/ Math/PI 2))
           (mag z) 1))
       (let [z (complex-from-cartesian -5 23)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  -5
           (im z)  23
           (arg z) 1.78485701
@@ -48,31 +48,31 @@
 
     (testing "from polar coordinates"
       (let [z (complex-from-polar 0 0)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  0
           (arg z) 0
           (mag z) 0))
       (let [z (complex-from-polar (/ Math/PI 2) 1)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  1
           (arg z) (/ Math/PI 2)
           (mag z) 1))
       (let [z (complex-from-polar Math/PI 1)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  -1
           (im z)  0
           (arg z) Math/PI
           (mag z) 1))
       (let [z (complex-from-polar (- (/ Math/PI 2)) 1)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  0
           (im z)  -1
           (arg z) (- (/ Math/PI 2))
           (mag z) 1))
       (let [z (complex-from-polar 1025.78 123)]
-        (are [x y] (close? x y)
+        (are-close
           (re z)  -6.147266197
           (im z)  122.8462906
           (arg z) (mod 1025.78 (* Math/PI 2))
@@ -81,27 +81,27 @@
   (testing "add"
     (testing "two complex numbers"
       (let [z (add (complex-from-cartesian 3 6) (complex-from-cartesian 2.5 -8))]
-        (are [x y] (close? x y)
+        (are-close
           (re z) 5.5
           (im z) -2))))
 
   (testing "subtract"
     (testing "two complex numbers"
       (let [z (sub (complex-from-cartesian 10 4) (complex-from-cartesian -3 2.2))]
-        (are [x y] (close? x y)
+        (are-close
           (re z) 13
           (im z) 1.8))))
 
   (testing "multiply"
     (testing "two complex numbers"
       (let [z (mul (complex-from-polar 0.22 5) (complex-from-polar 0.3 6))]
-        (are [x y] (close? x y)
+        (are-close
           (arg z) 0.52
           (mag z) 30))))
 
   (testing "divide"
     (testing "two complex numbers"
       (let [z (div (complex-from-polar 0.1 9) (complex-from-polar 0.3 3))]
-        (are [x y] (close? x y)
+        (are-close
           (arg z) -0.2
           (mag z) 3)))))
--- test/complex_grapher/test_utils.clj
+++ test/complex_grapher/test_utils.clj
@@ -1,9 +1,29 @@
-(ns complex-grapher.test-utils)
+(ns complex-grapher.test-utils
+  (:require [cljs.test :refer [is are]]))
+
+(def default-delta 0.00001)
 
 (defmacro close?
   "Check if `x` and `y` are within `delta` of one another.
-  If `delta` is not given, it is 0.00001 by default."
+  If `delta` is not given, `complex-grapher.test-utils/default-delta` is used instead."
   ([x y]
-   `(close? ~x ~y 0.00001))
+   `(close? ~x ~y ~default-delta))
   ([x y delta]
    (list '< (list 'Math/abs (list '- x y)) delta)))
+
+(defmacro is-close
+  "Assert that `x` and `y` are within `delta` of one another.
+  If `delta` is not given, `complex-grapher.test-utils/default-delta` is used instead."
+  ([x y]
+   `(is (close? ~x ~y)))
+  ([x y delta]
+   `(is (close? ~x ~y ~delta))))
+
+(defmacro are-close
+  "Assert that each pair of values is within `delta` of each other, where `delta` is the first value given.
+  There must be an even number of remaining forms after `delta`.
+  If `delta` is not given, `complex-grapher.test-utils/default-delta` is used instead."
+  ([& forms]
+   (if (even? (count forms))
+     `(are [x y] (close? x y) ~@forms)
+     `(are [x y] (close? x y ~(first forms)) ~@(rest forms)))))
