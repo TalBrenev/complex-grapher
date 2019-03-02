6e66571954bc9ccce6740d4ef20e983c0fbf7b8e
Tal Brenev <tal.brenev@gmail.com>
2019-03-01 22:31:22 -0500

    Update version

--- project.clj
+++ project.clj
@@ -16,10 +16,11 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" complex-grapher.test-runner]}
 
+  :plugins [[cider/cider-nrepl "0.21.1"]]
+
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.0"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
                                   [cider/piggieback "0.4.0"]]
-                   :plugins [[cider/cider-nrepl "0.20.0"]]
                    :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
                    :clean-targets ^{:protect false} ["resources/public/cljs-out"
                                                      :target-path]}})
