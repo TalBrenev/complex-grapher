8858146adf385c38725075f252816cc908ecc417
Tal Brenev <tal.brenev@gmail.com>
2020-04-11 11:22:17 -0400

    Refactor

--- src/complex_grapher/core.cljs
+++ src/complex_grapher/core.cljs
@@ -2,14 +2,17 @@
     (:require [complex-grapher.complex-arithmetic :refer [complex-from-cartesian add re im]]
               [complex-grapher.parser :refer [parse]]
               [complex-grapher.canvas :refer [fix-size width height]]
-              [complex-grapher.webgl :refer [draw]]))
+              [complex-grapher.webgl :refer [draw]]
+              [complex-grapher.utils :refer [get-value set-value add-event-listener]]))
 
 (enable-console-print!)
 
 (def canvas-id "canvas")
 
-(defonce graph-state (atom {:centre (complex-from-cartesian 0 0)
-                            :zoom   0.01}))
+(defonce graph-state (atom {:centre   (complex-from-cartesian 0 0)
+                            :zoom     0.01
+                            :function "z"
+                            :modulus  0.5}))
 
 (defn top-left-corner [centre zoom]
   (add centre (complex-from-cartesian (- (* 0.5 zoom (width canvas-id)))
@@ -19,24 +22,13 @@
   (add centre (complex-from-cartesian (* 0.5 zoom (width canvas-id))
                                       (- (* 0.5 zoom (height canvas-id))))))
 
-(defn get-modulus []
-  (-> js/document
-      (.getElementById "modulus")
-      (.-value)
-      (js/parseFloat)))
-
-(defn get-function []
-  (-> js/document
-      (.getElementById "function")
-      (.-value)))
-
-(defn draw-graph []
-  (let [{:keys [centre zoom]} @graph-state
+(defn draw-graph [state]
+  (let [{:keys [centre zoom function modulus]} state
         top-left (top-left-corner centre zoom)
         bottom-right (bottom-right-corner centre zoom)]
     (draw canvas-id
-          (parse (get-function))
-          (get-modulus)
+          (parse function)
+          modulus
           (re top-left)
           (im top-left)
           (re bottom-right)
@@ -44,9 +36,17 @@
 
 (defn setup []
   (fix-size canvas-id)
-  (-> js/document
-      (.getElementById "graphbutton")
-      (.-firstChild)
-      (.addEventListener "click" draw-graph)))
+
+  (set-value "function" (:function @graph-state))
+  (set-value "modulus" (:modulus @graph-state))
+
+  (add-event-listener "function" "input" #(swap! graph-state assoc :function (get-value "function")))
+  (add-event-listener "modulus" "input" #(swap! graph-state assoc :modulus (get-value "modulus")))
+
+  (add-watch graph-state :drawer
+    (fn [_ _ _ new-state]
+      (draw-graph new-state)))
+
+  (draw-graph @graph-state))
 
 (.addEventListener js/window "load" setup)
--- src/complex_grapher/utils.cljs
+++ src/complex_grapher/utils.cljs
@@ -7,3 +7,19 @@
 (defn in? [item coll]
   "Returns `true` if `item` is in `coll`, otherwise returns `nil`."
   (some #(= item %) coll))
+
+(defn get-element [id]
+  "Gets the element with the given id in the DOM."
+  (.getElementById js/document id))
+
+(defn get-value [element-id]
+  "Gets the value property of an element in the DOM."
+  (.-value (get-element element-id)))
+
+(defn set-value [element-id value]
+  "Sets the value property of an element in the DOM."
+  (set! (.-value (get-element element-id)) value))
+
+(defn add-event-listener [element-id event handler]
+  "Adds an event listener to an element in the DOM."
+  (.addEventListener (get-element element-id) event handler))
