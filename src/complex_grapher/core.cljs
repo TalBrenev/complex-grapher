fc8f149d0330b937b9b526516e7a178356704458
Tal Brenev <tal.brenev@gmail.com>
2020-04-11 17:45:41 -0400

    Refactor

--- src/complex_grapher/core.cljs
+++ src/complex_grapher/core.cljs
@@ -7,6 +7,17 @@
 (enable-console-print!)
 
 (defonce canvas-id "canvas")
+(defonce top-left-id "topleft")
+(defonce bottom-right-id "bottomright")
+(defonce overlay-id "error")
+(defonce function-id "function")
+(defonce modulus-id "modulus")
+(defonce zoom-in-id "zoomin")
+(defonce zoom-out-id "zoomout")
+(defonce shift-up-id "shiftup")
+(defonce shift-down-id "shiftdown")
+(defonce shift-right-id "shiftright")
+(defonce shift-left-id "shiftleft")
 
 (defonce graph-state (atom {:centre   (complex-from-cartesian 0 0)
                             :zoom     0.01
@@ -22,10 +33,10 @@
                                       (- (* 0.5 zoom (height canvas-id))))))
 
 (defn show-error-overlay []
-  (set! (.-style (get-element "error")) "opacity: 1"))
+  (set! (.-style (get-element overlay-id)) "opacity: 1"))
 
 (defn hide-error-overlay []
-  (set! (.-style (get-element "error")) "opacity: 0"))
+  (set! (.-style (get-element overlay-id)) "opacity: 0"))
 
 (defn draw-graph
   ([]
@@ -35,8 +46,8 @@
      (let [{:keys [centre zoom function modulus]} state
            top-left (top-left-corner centre zoom)
            bottom-right (bottom-right-corner centre zoom)]
-       (set! (.-innerText (get-element "topleft")) (complex->str top-left))
-       (set! (.-innerText (get-element "bottomright")) (complex->str bottom-right))
+       (set! (.-innerText (get-element top-left-id)) (complex->str top-left))
+       (set! (.-innerText (get-element bottom-right-id)) (complex->str bottom-right))
        (draw canvas-id
              (parse function)
              modulus
@@ -51,21 +62,21 @@
 (defn setup []
   (fix-size canvas-id)
 
-  (set-value "function" (:function @graph-state))
-  (set-value "modulus" (:modulus @graph-state))
+  (set-value function-id (:function @graph-state))
+  (set-value modulus-id (:modulus @graph-state))
 
-  (add-event-listener "function" "input" #(swap! graph-state assoc :function (get-value "function")))
-  (add-event-listener "modulus" "input" #(swap! graph-state assoc :modulus (get-value "modulus")))
-  (add-event-listener "zoomin" "click" #(swap! graph-state update :zoom / 2))
-  (add-event-listener "zoomout" "click" #(swap! graph-state update :zoom * 2))
-  (add-event-listener "shiftup" "click" #(swap! graph-state (fn [state]
-                                                              (update state :centre sub (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
-  (add-event-listener "shiftdown" "click" #(swap! graph-state (fn [state]
-                                                                (update state :centre add (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
-  (add-event-listener "shiftleft" "click" #(swap! graph-state (fn [state]
-                                                                (update state :centre sub (* 0.3 (width canvas-id) (:zoom state))))))
-  (add-event-listener "shiftright" "click" #(swap! graph-state (fn [state]
-                                                                 (update state :centre add (* 0.3 (width canvas-id) (:zoom state))))))
+  (add-event-listener function-id "input" #(swap! graph-state assoc :function (get-value function-id)))
+  (add-event-listener modulus-id "input" #(swap! graph-state assoc :modulus (get-value modulus-id)))
+  (add-event-listener zoom-in-id "click" #(swap! graph-state update :zoom / 2))
+  (add-event-listener zoom-out-id "click" #(swap! graph-state update :zoom * 2))
+  (add-event-listener shift-up-id "click" #(swap! graph-state (fn [state]
+                                                               (update state :centre sub (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
+  (add-event-listener shift-down-id "click" #(swap! graph-state (fn [state]
+                                                                  (update state :centre add (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
+  (add-event-listener shift-left-id "click" #(swap! graph-state (fn [state]
+                                                                  (update state :centre sub (* 0.3 (width canvas-id) (:zoom state))))))
+  (add-event-listener shift-right-id "click" #(swap! graph-state (fn [state]
+                                                                   (update state :centre add (* 0.3 (width canvas-id) (:zoom state))))))
 
   (add-watch graph-state :drawer
     (fn [_ _ _ new-state]
