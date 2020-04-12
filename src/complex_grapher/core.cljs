722a81294366131d8d26f6e84684446d0732daa2
Tal Brenev <tal.brenev@gmail.com>
2020-04-12 11:21:14 -0400

    Split setup into many functions

--- src/complex_grapher/core.cljs
+++ src/complex_grapher/core.cljs
@@ -6,6 +6,7 @@
 
 (enable-console-print!)
 
+;; Ids of elements in the DOM
 (defonce canvas-id "canvas")
 (defonce top-left-id "topleft")
 (defonce bottom-right-id "bottomright")
@@ -23,16 +24,22 @@
 (defonce controls-btn-id "ctr-show")
 (defonce no-webgl-id "no-webgl-wrapper")
 
+;; How much the graph shifts/zooms by when the zoom buttons are used
+(defonce zoom-factor 2)
+(defonce shift-factor 0.3)
+
 (defonce graph-state (atom {:centre   (complex-from-cartesian 0 0)
                             :zoom     0.01
                             :function "z"
                             :modulus  0.5}))
 
 (defn top-left-corner [centre zoom]
+  "Computes the complex number represented by the top-left corner of the graph."
   (add centre (complex-from-cartesian (- (* 0.5 zoom (width canvas-id)))
                                       (* 0.5 zoom (height canvas-id)))))
 
 (defn bottom-right-corner [centre zoom]
+  "Computes the complex number represented by the bottom-right corner of the graph."
   (add centre (complex-from-cartesian (* 0.5 zoom (width canvas-id))
                                       (- (* 0.5 zoom (height canvas-id))))))
 
@@ -63,56 +70,78 @@
      (catch :default e
        (show-error-overlay)))))
 
+(defn zoom-in []
+  (swap! graph-state update :zoom / 2))
+
+(defn zoom-out []
+  (swap! graph-state update :zoom * 2))
+
+(defn shift-up []
+  (swap! graph-state #(update % :centre sub (mul (* shift-factor (height canvas-id) (:zoom %)) i))))
+
+(defn shift-down []
+  (swap! graph-state #(update % :centre add (mul (* shift-factor (height canvas-id) (:zoom %)) i))))
+
+(defn shift-left []
+  (swap! graph-state #(update % :centre sub (* shift-factor (width canvas-id) (:zoom %)))))
+
+(defn shift-right []
+  (swap! graph-state #(update % :centre add (* shift-factor (width canvas-id) (:zoom %)))))
+
+(defn graph-location [x y]
+  (let [{:keys [centre zoom]} @graph-state
+        top-left (top-left-corner centre zoom)
+        bottom-right (bottom-right-corner centre zoom)
+        graph-range (sub bottom-right top-left)]
+    (complex->str
+      (complex-from-cartesian
+        (+ (re top-left) (* (/ x (width canvas-id)) (re graph-range)))
+        (+ (im top-left) (* (/ y (height canvas-id)) (im graph-range)))))))
+
+(defn toggle-controls []
+  (if (= (.-innerText (get-element controls-btn-id)) "Show Controls")
+    (do
+      (set! (.-innerText (get-element controls-btn-id)) "Hide Controls")
+      (set! (.-style (get-element controls-id)) "bottom: 0px"))
+    (do
+      (set! (.-innerText (get-element controls-btn-id)) "Show Controls")
+      (set! (.-style (get-element controls-id)) ""))))
+
+(defn show-no-webgl []
+  (set! (.-style (get-element no-webgl-id)) "visibility: visible"))
+
+(defn setup-default-values []
+  (set-value function-id (:function @graph-state))
+  (set-value modulus-id (:modulus @graph-state)))
+
+(defn setup-ui-events []
+  (add-event-listener function-id "input" #(swap! graph-state assoc :function (get-value function-id)))
+  (add-event-listener modulus-id "input" #(swap! graph-state assoc :modulus (get-value modulus-id)))
+  (add-event-listener zoom-in-id "click" zoom-in)
+  (add-event-listener zoom-out-id "click" zoom-out)
+  (add-event-listener shift-up-id "click" shift-up)
+  (add-event-listener shift-down-id "click" shift-down)
+  (add-event-listener shift-left-id "click" shift-left)
+  (add-event-listener shift-right-id "click" shift-right)
+  (add-event-listener canvas-id "mouseover" #(set! (.-style (get-element location-id)) "visibility: visible"))
+  (add-event-listener canvas-id "mouseout" #(set! (.-style (get-element location-id)) "visibility: hidden"))
+  (add-event-listener canvas-id "mousemove" #(set! (.-innerText (get-element location-id)) (graph-location (.-layerX %) (.-layerY %))))
+  (add-event-listener controls-btn-id "click" toggle-controls))
+
+(defn setup-graph []
+  (fix-size canvas-id)
+  (add-watch graph-state :drawer
+    (fn [_ _ _ new-state]
+      (draw-graph new-state)))
+  (.addEventListener js/window "resize" #(do (fix-size canvas-id) (draw-graph)))
+  (draw-graph))
+
 (defn setup []
   (if (detect-webgl canvas-id)
     (do
-      (fix-size canvas-id)
-
-      (set-value function-id (:function @graph-state))
-      (set-value modulus-id (:modulus @graph-state))
-
-      (add-event-listener function-id "input" #(swap! graph-state assoc :function (get-value function-id)))
-      (add-event-listener modulus-id "input" #(swap! graph-state assoc :modulus (get-value modulus-id)))
-      (add-event-listener zoom-in-id "click" #(swap! graph-state update :zoom / 2))
-      (add-event-listener zoom-out-id "click" #(swap! graph-state update :zoom * 2))
-      (add-event-listener shift-up-id "click" #(swap! graph-state (fn [state]
-                                                                   (update state :centre sub (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
-      (add-event-listener shift-down-id "click" #(swap! graph-state (fn [state]
-                                                                      (update state :centre add (mul (* 0.3 (height canvas-id) (:zoom state)) i)))))
-      (add-event-listener shift-left-id "click" #(swap! graph-state (fn [state]
-                                                                      (update state :centre sub (* 0.3 (width canvas-id) (:zoom state))))))
-      (add-event-listener shift-right-id "click" #(swap! graph-state (fn [state]
-                                                                       (update state :centre add (* 0.3 (width canvas-id) (:zoom state))))))
-
-      (add-event-listener canvas-id "mouseover" #(set! (.-style (get-element location-id)) "visibility: visible"))
-      (add-event-listener canvas-id "mouseout" #(set! (.-style (get-element location-id)) "visibility: hidden"))
-      (add-event-listener canvas-id "mousemove" #(let [x (.-layerX %)
-                                                       y (.-layerY %)
-                                                       {:keys [centre zoom]} @graph-state
-                                                       top-left (top-left-corner centre zoom)
-                                                       bottom-right (bottom-right-corner centre zoom)]
-                                                   (set!
-                                                     (.-innerText (get-element location-id))
-                                                     (complex->str
-                                                       (complex-from-cartesian
-                                                         (+ (re top-left) (* (/ x (width canvas-id)) (re (sub bottom-right top-left))))
-                                                         (+ (im top-left) (* (/ y (height canvas-id)) (im (sub bottom-right top-left)))))))))
-
-      (add-event-listener controls-btn-id "click" #(if (= (.-innerText (get-element controls-btn-id)) "Show Controls")
-                                                     (do
-                                                       (set! (.-innerText (get-element controls-btn-id)) "Hide Controls")
-                                                       (set! (.-style (get-element controls-id)) "bottom: 0px"))
-                                                     (do
-                                                       (set! (.-innerText (get-element controls-btn-id)) "Show Controls")
-                                                       (set! (.-style (get-element controls-id)) ""))))
-
-      (add-watch graph-state :drawer
-        (fn [_ _ _ new-state]
-          (draw-graph new-state)))
-
-      (.addEventListener js/window "resize" #(do (fix-size canvas-id) (draw-graph)))
-
-      (draw-graph))
-    (set! (.-style (get-element no-webgl-id)) "visibility: visible")))
+      (setup-default-values)
+      (setup-ui-events)
+      (setup-graph))
+    (show-no-webgl)))
 
 (.addEventListener js/window "load" setup)
