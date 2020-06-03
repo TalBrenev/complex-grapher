0980a611454bd4a12f718ce6a78a734bafa27e1f
Tal Brenev <tal.brenev@gmail.com>
2020-06-02 23:09:41 -0400

    Fix invalidation path

--- .github/deploy/run.sh
+++ .github/deploy/run.sh
@@ -9,6 +9,6 @@ aws s3 sync ./build "s3://${AWS_S3_BUCKET_NAME}" --delete
 
 echo "Invalidating CloudFront cache..."
 
-aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} --paths /*
+aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
 
 echo "Done!"
