776375ab8006328210ef235abaa3a2c5a2976793
Tal Brenev <tal.brenev@gmail.com>
2020-07-04 18:28:34 -0400

    Refactor textbox

--- src/complex_grapher/ui/textbox.cljs
+++ src/complex_grapher/ui/textbox.cljs
@@ -2,6 +2,15 @@
     (:require [reagent.core :as r]))
 
 (defn textbox [value]
-  [:input {:type "textbox"
-           :value @value
-           :onInput #(reset! value (-> % (.-target) (.-value)))}])
+  (let [textbox-node (r/atom nil)]
+    (add-watch value
+               :textbox
+               (fn [_ _ _ new-value]
+                 (if-let [textbox-node @textbox-node]
+                   (if-not (= (.-value textbox-node) new-value)
+                     (aset textbox-node "value" new-value)))))
+    (fn []
+      [:input {:ref #(reset! textbox-node %)
+               :type "textbox"
+               :defaultValue @value
+               :onInput #(reset! value (-> % (.-target) (.-value)))}])))
