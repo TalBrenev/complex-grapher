82c138d44423f1cb4e7b1b8d995e9e58efa491dd
Tal Brenev <tal.brenev@gmail.com>
2022-05-17 13:57:42 -0400

    Improve mobile layout

--- src/complex_grapher/ui/menu.cljs
+++ src/complex_grapher/ui/menu.cljs
@@ -5,19 +5,20 @@
 (defn menu [graph-state initial-graph-state]
   (let [show (r/atom true)]
     (fn []
-      [:div {:class "menu"}
-        [:div.function
-          [:div.input-wrapper
-            [:label "Function:"]
-            [textbox (r/cursor graph-state [:function])]]]
-        [:div.button-wrapper
-          [:button {:onClick #(reset! graph-state initial-graph-state)} "Reset"]
-          [:button "Help"]
-          [:button "About"]]
-        [:div.hide-button-wrapper
-          [:button {:onClick #(let [menu (-> % (.-target) (.-parentElement) (.-parentElement))]
-                                (if @show
-                                  (set! (-> menu (.-style) (.-top)) (str "-" (.-clientHeight menu) "px"))
-                                  (set! (-> menu (.-style) (.-top)) ""))
-                                (reset! show (not @show)))}
-            (if @show "Hide" "Show")]]])))
+      [:div.menu
+        [:div.menu-wrapper
+          [:div.function
+            [:div.input-wrapper
+              [:label "Function:"]
+              [textbox (r/cursor graph-state [:function])]]]
+          [:div.button-wrapper
+            [:button {:onClick #(reset! graph-state initial-graph-state)} "Reset"]
+            [:button "Help"]
+            [:button "About"]]
+          [:div.hide-button-wrapper
+            [:button {:onClick #(let [menu (-> % (.-target) (.-parentElement) (.-parentElement) (.-parentElement))]
+                                  (if @show
+                                    (set! (-> menu (.-style) (.-top)) (str "-" (.-clientHeight menu) "px"))
+                                    (set! (-> menu (.-style) (.-top)) ""))
+                                  (reset! show (not @show)))}
+              (if @show "Hide" "Show")]]]])))
